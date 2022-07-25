806d1ec18651ac78b42ffa32787fdf7f9833dd3b
Aidan <aidanjoyce12@gmail.com>
2022-07-25 19:21:15 -0400

    added for loop questions into index.html

--- index.js
+++ index.js
@@ -2,4 +2,25 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for (i = 0; i < weeklyExpenseQuestions.length; i++){
+    
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+    console.log(answer)
+}
+
+for (i = 0; i < monthlyExpenseQuestions.length; i++) {
+
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+    console.log(answer)
+}
+
+for (i = 0; i < annualExpenseQuestions.length; i++) {
+
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+    console.log(answer)
+}
+
+
